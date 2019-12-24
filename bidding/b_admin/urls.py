1907fb6368fefa2926ed68c8e3a1bab3a55d43ef
mahendharone1 <59193714+mahendharone1@users.noreply.github.com>
2019-12-24 15:15:37 +0530

    space

--- bidding/b_admin/urls.py
+++ bidding/b_admin/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-
 from b_admin import views
 
 urlpatterns = [
