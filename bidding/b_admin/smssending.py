cbb907ba49ae397f65244ed77f2e2d400e46501a
mahendharone1 <59193714+mahendharone1@users.noreply.github.com>
2019-12-24 15:16:24 +0530

    requests

--- bidding/b_admin/smssending.py
+++ bidding/b_admin/smssending.py
@@ -1,11 +1,7 @@
 import requests
 
-
 def sendSMS(contact_no, message):
-    import requests
-
     url = "https://www.fast2sms.com/dev/bulk"
-
     payload = "sender_id=FSTSMS&message="+message+"&language=english&route=p&numbers="+contact_no
     headers = {
         'authorization': "knkt8ihCfOKPQ6qkfyVxR0DSNJcB1gNPDdeYHc4jE4g13z4zMa54HzMQOaDB",
